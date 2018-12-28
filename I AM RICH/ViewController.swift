f5e795a12f3249619030a759c6ba6a3f2075164e
Noi <starrpatty28@gmail.com>
2018-12-27 22:20:13 -0800

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Alicia Livingston on 12/27/18.
+//  Copyright © 2018 Alicia Livingston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
